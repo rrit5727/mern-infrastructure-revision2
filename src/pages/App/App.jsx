4a11f99404e07e52e20c877f4c339f28d1c22a65
rrit5727 <rrit5727@gmail.com>
2025-04-09 15:44:24 +1000

    Clean up and organize React app structure, remove unnecessary files, and set up Express server with dotenv and mongoose integration

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='App'>
+        App
+      </main>
+    </>
+  )
+}
+
+export default App
