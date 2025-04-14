01638b442204bfcc944f090f2e5f0b5b78f7b514
rrit5727 <rrit5727@gmail.com>
2025-04-14 14:31:04 +1000

    Set up User model and added token-based authentication using JWT

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,14 +1,28 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
 
 
 module.exports = {
   create
 }
 
-function create(req, res) {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  })
+async function create(req, res) {
+
+  try { 
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json(token);
+  } catch (err) { 
+    res.status(400).json(err)
+  }
+}
+
+// Helper functions
+function createJWT(user) {
+
+  return jwt.sign(
+    {user},
+    process.env.SECRET,
+    {expiresIn: '24h'}
+  )
 }
